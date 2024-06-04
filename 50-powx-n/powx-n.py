a8495c763f896d6b14f69f6eb9eb74c39bc7bb87
CimrikaBaj <98899661+cimrikabaj@users.noreply.github.com>
2024-06-04 09:46:00 +0545

    Time: 38 ms (36.32%) | Memory: 16.5 MB (93.70%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,22 +1,16 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        b = n
-        if x == 0:
-            return 0
-        elif b == 0:
-            return 1
-        elif b < 0:
-            b = -b
+        if n < 0:
             x = 1 / x
-        
-        a = 1
-        while b > 0:
-            if b % 2 == 0:
-                x = x * x
-                b = b // 2
-            else:
-                b = b - 1
-                a = a * x
-                
-        return a
-        
+            n = -n
+    
+        result = 1
+        current_product = x
+    
+        while n > 0:
+            if n % 2 == 1:  # If n is odd
+                result *= current_product
+            current_product *= current_product  # Square the base
+            n //= 2  # Halve the exponent
+    
+        return result
